9c091e5396dfc0ced889c0c3d97e06ea123aa5e6
afr114 <afr114@gmail.com>
2016-02-25 01:03:24 -0800

    add textContainter component

---
+++ src/client/components/Bootstrap/TextContainer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import * as ReactBootstrap from 'react-bootstrap';
+
+class TextContainer extends Component {
+  render(){
+    return (
+      <div className="container">
+      <h1></h1>
+      <p className="text-left"></p>
+    );
+  }
+}
+
+export default TextContainer;
