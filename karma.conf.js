79df0db1d7c838b27159ca11953c0f0614fe6276
Sanjuro Jogdeo <weisenheimer@sanjuro.net>
2016-04-11 19:54:29 -0700

    Added mocha.opts
    The test is working ok with ./node-modules/.bin/mocha but I can't get karma to work.

--- karma.conf.js
+++ karma.conf.js
@@ -12,12 +12,20 @@ module.exports = function(config) {
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha'],
 
-
+    
     // list of files / patterns to load in the browser
     files: [
-      'tests'
+      'test/*.js'
     ],
 
+    
+    // client: {
+    //   mocha: {
+    //     reporter: 'html' // change Karma's debug.html to the mocha web reporter
+
+        
+    //   }
+    // },
 
     // list of files to exclude
     exclude: [
@@ -46,7 +54,7 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
 
 
     // enable / disable watching file and executing tests whenever any file changes
@@ -55,12 +63,12 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'Chrome'],
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
