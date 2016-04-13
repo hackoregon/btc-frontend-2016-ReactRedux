cd2bdd1f7d49a7d6195e9e8d57264c8c8b7fecaf
Sanjuro Jogdeo <weisenheimer@sanjuro.net>
2016-04-12 18:26:43 -0700

    Add babel preprocessor

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha','chai'],
 
     
     // list of files / patterns to load in the browser
@@ -35,7 +35,9 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        "test/*.js": ["babel"]
     },
+
 
 
     // test results reporter to use
