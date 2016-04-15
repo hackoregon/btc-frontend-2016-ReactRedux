3803c7ca23f49e8a6fac025576a2557a286ba73c
Austin Valeske <avaleske@gmail.com>
2016-04-15 14:47:16 -0700

    fixing bug in express that prevented anything but the home page from showing

--- devServer.js
+++ devServer.js
@@ -13,7 +13,7 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
