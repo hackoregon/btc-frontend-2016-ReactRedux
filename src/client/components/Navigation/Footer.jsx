3ec279bc156e94604a89ab658afd5508f864ad9f
Sanjuro Jogdeo <weisenheimer@sanjuro.net>
2016-06-20 07:32:55 -0700

    Fix twitter link

--- src/client/components/Navigation/Footer.jsx
+++ src/client/components/Navigation/Footer.jsx
@@ -60,7 +60,7 @@ render(){
                           sm={ 1 }
                           lg={ 1 }
                           params={ this.props.params }>
-                      <a className="footer-link" href="#" style={linkStyle} params={ this.props.params }><span params={ this.props.params }>Twitter</span></a>
+                      <a className="footer-link" href="https://twitter.com/HackOregon" target="_blank" style={linkStyle} params={ this.props.params }><span params={ this.props.params }>Twitter</span></a>
                       </Col>
 
                       <Col xs={ 2 }
@@ -68,7 +68,7 @@ render(){
                           sm={ 1 }
                           lg={ 1 }
                           params={ this.props.params }>
-                      <a className="footer-link" href="https://github.com/hackoregon" target="blank" style={linkStyle} params={ this.props.params }><span params={ this.props.params }>GitHub</span></a>
+                      <a className="footer-link" href="https://github.com/hackoregon" target="_blank" style={linkStyle} params={ this.props.params }><span params={ this.props.params }>GitHub</span></a>
                       </Col>
                       
 
