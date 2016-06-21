7e39dacb9f85eb9c7894ea960b66cdd5a624ee24
DavideDaniel <davidedaniel@gmail.com>
2016-06-20 19:24:54 -0700

    about page adjusted

--- src/client/routes/AboutPage.js
+++ src/client/routes/AboutPage.js
@@ -1,7 +1,7 @@
-import React, { Component, PropTypes } from 'react';
-import { Grid,Col,Row } from 'react-bootstrap';
-import SearchResultsForm from '../containers/SearchResults/SearchResultsForm.jsx';
+import React, {Component, PropTypes} from 'react';
+import {Grid, Col, Row} from 'react-flexbox-grid';
 import AboutPortraits from '../containers/AboutPortraits.jsx';
+import Footer from '../components/Navigation/Footer.jsx';
 import TextContainer from '../components/Bootstrap/TextContainer.jsx';
 import BTCNav from '../components/Navigation/BTCNav.jsx';
 
@@ -22,13 +22,31 @@ class AboutPage extends Component {
             'Marcus Estes',
             'John Cohoon',
             'John Streater',
-            'Colby Aley'
+            'Colby Aley',
+            'Hobson Lane',
+'Daniel Miller',
+'Thunder Shiviah',
+'Allen Grimm',
+'Cole Howard',
+'David Garber ',
+'Lewis Notestine ',
+'Ryan Miller  ',
+'Sam Higgins ',
+'Marie Nunez-Mendez',
+'Riley Rustael',
+'Dan Wood ',
+'Monty Rhode ',
+'Barrett Paul '
         ];
 
         var returnThis = [];
-        for (var i=0; i < names.length; i++) {
-            const name1 = names[i] ? names[i++] : '';
-            const name2 = names[i] ? names[i] : '';
+        for (var i = 0; i < names.length; i++) {
+            const name1 = names[i]
+                ? names[i++]
+                : '';
+            const name2 = names[i]
+                ? names[i]
+                : '';
             returnThis.push(
                 <Row key={i} className="show-grid">
                     <Col xs={3} xsOffset={3} className="text-center">{name1}</Col>
@@ -44,78 +62,72 @@ class AboutPage extends Component {
             </Grid>
         )
 
-
     }
 
     render() {
         return (
-            <div>
-                <BTCNav />
-                <Grid fluid={ true }
-                      style={ {    marginTop: '3rem',    fontWeight: '200px'} }
+          <div {...this.props} style={{display:'flex',minHeight:'100vh',flexDirection:'column'}}>
+            <BTCNav ref={'nav'} onToggleSelect={this.handleSelect}/>
+            <div style={{flex:'1',paddingTop:'2rem'}}>
+
+                <Grid center='xs' fluid={ true }
                       params={ this.props.params }>
-                </Grid>
 
                 <div className="container section ng-scope">
                     <TextContainer>
                         <h1>This Project Was Built by 100% Volunteers</h1>
                     </TextContainer>
                     <TextContainer divClasses="text-left lead">
-                        Hack Oregon is a community-powered non-profit building civic
-                         data projects on different themes to promote engagement,
-                         awareness, and quality of life.
+                        Hack Oregon is a community-powered non-profit building civic data projects on different themes to promote engagement, awareness, and quality of life.
                     </TextContainer>
                     <TextContainer divClasses="text-left">
-                    All Hack Oregon projects are open source, built entirely by
-                     volunteers from our local community. That means that if you
-                     live in the Oregon area, or would like to contribute remotely,
-                     you can work with us!<br/><br/>
+                        All Hack Oregon projects are open source, built entirely by volunteers from our local community. That means that if you live in the Oregon area, or would like to contribute remotely, you can work with us!<br/><br/>
 
-                    Although each of our projects require a slightly different blend
-                     of talents and resources, our teams are always interdisciplinary
-                     and always have roles available for people at all levels of experience.
-                     We place our team members by balancing the skills they want to contribute
-                     with the skill they want to learn — and by operating outside of normal bureaucratic,
-                     client-based, or venture funded restrictions — we're free to move fast and innovate faster.<br/><br/>
+                        Although each of our projects require a slightly different blend of talents and resources, our teams are always interdisciplinary and always have roles available for people at all levels of experience. We place our team members by balancing the skills they want to contribute with the skill they want to learn — and by operating outside of normal bureaucratic, client-based, or venture funded restrictions — we're free to move fast and innovate faster.<br/><br/>
 
-                     It's not always easy, but we think it's pretty worth it.<br/><br/>
+                        It's not always easy, but we think it's pretty worth it.<br/><br/>
 
-                    Find out more at <a href="http://hackoregon.org">hackoregon.org</a><br/><br/>
-                    </TextContainer>
-                    <TextContainer>
-                        <h1 className="text-center grayfont">The Team</h1>
+                        Find out more at
+                        <a href="http://hackoregon.org">hackoregon.org</a><br/><br/>
                     </TextContainer>
 
-                    <AboutPortraits />
+                      <Col xs={12} around = 'xs' around='xs'>
+                        <h1 xs className="text-center grayfont">The Team</h1>
+                        <h4 xs >Cat Nikolovski - Producer</h4>
+                        <h4 xs>James Ofsink — Researcher and Facilitator</h4>
+                        <h4 xs>Ken Whaler - UX & Visual design</h4>
+                        <h4 xs>David Daniel — Full Stack Dev</h4>
+                        <h4 xs>Sanjuro Jogdeo — Full Stack Dev</h4>
+                      </Col>
 
-                    <br/><br/>
-
-                    <TextContainer>
-                        <h2 className="text-center grayfont">Team Members Not Pictured</h2>
-                    </TextContainer>
+                        <h2 className="text-center grayfont">Special Thanks to our past Behind the Curtain Team Members!</h2>
 
                     <TextContainer divClasses="text-center">
-                        <h4>Ryan Miller — Front End Team</h4>
-                        <h4>Sam Higgins — Data Scientist</h4>
-                        <h4>Marie Nunez-Mendez — Researcher and Facilitator</h4>
-                        <h4>Riley Rustael — Back End Team</h4>
-                        <h4>Dan Wood — Front End Team</h4>
-                        <h4>Monty Rhode — Front End Team</h4>
-                        <h4>Barrett Paul — Product Design</h4>
-                    </TextContainer>
 
-                    <br/><br/>
 
-                    <TextContainer>
-                        <h2 className="text-center grayfont">Special Thanks to our past Behind the Curtain Team Members!</h2>
+
+
+
+
+
+
+
+
+
+
+
+
+
                     </TextContainer>
                     {this.buildSpecialThanks()}
 
                 </div>
 
-
+              </Grid>
+              </div>
+                <Footer style={{flex: '1'}}/>
             </div>
-            );
+        );
     }
 }
 
