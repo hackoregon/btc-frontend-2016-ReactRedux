cb5c2965111914340c6ec76529d5096eee66ba84
DavideDaniel <davidedaniel@gmail.com>
2016-09-19 19:42:03 -0700

    remove devServer

--- live-dev-server.js
+++ live-dev-server.js
@@ -6,19 +6,23 @@ const webpack = require('webpack');
 const config = require('./build-conf/webpack.config-dev');
 const app = express();
 const compiler = webpack(config);
-// const port = 3000;
-const port = 80; // backup incase
+const port = 3000;
+// const port = 80; // backup incase
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
+if(process.env.NODE_ENV === 'development') {
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  }));
+  app.use(require('webpack-hot-middleware')(compiler));
+}
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(port, function(err) {
+  console.log(process.env.NODE_ENV,' active');
   if (err) {
     console.log(err);
   }
